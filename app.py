9afccb51f426306b1074dab028abd7a5f7c23f87
Pranav A S <124978526+pranavsivakumar01@users.noreply.github.com>
2024-11-18 09:55:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)
+
+
+file_path = r'C:\Users\Pranav sivakumar\Desktop\bdamicro\NikeProductDescriptions.csv'
+data = pd.read_csv(file_path)
+
+@app.route('/search', methods=['POST'])
+def search_product():
+    title = request.json.get('title', '')
+    result = data[data['Title'].str.contains(title, case=False, na=False)]
+    if not result.empty:
+        return jsonify(result.to_dict(orient='records'))
+    else:
+        return jsonify([])
+
+if __name__ == '__main__':
+    app.run(port=5000)
